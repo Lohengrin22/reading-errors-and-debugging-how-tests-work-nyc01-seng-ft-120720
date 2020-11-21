26aeddab9fcf526df3617dedf2fc8233e9b87aaa
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-21 20:36:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
+first number = 1 
+second_number = 2 
+sum = first_number + second_number 
+difference = first_number - second_number 
+product = first_number * second_number 
+quotient = first_number / second_number 
 # Add your variables here
